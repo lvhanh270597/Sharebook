4d10201020e61fa16248329b746f359332f55129
lvhanh270597 <lvhanh.270597@gmail.com>
2018-11-20 16:15:39 +0700

    add

--- application/views/book_view.php
+++ application/views/book_view.php
@@ -47,29 +47,18 @@
                 <td><strong><?php echo $product['value']; ?>k</strong></td>                
               </tr>
             </tbody>
-          </table>          
-          <div class="dropdown bootstrap-select dropup"><select class="selectpicker" tabindex="-98">
-<option>Mustard</option>
-<option>Ketchup</option>
-<option>Relish</option>
-</select><button type="button" class="btn dropdown-toggle btn-light" data-toggle="dropdown" role="button" title="Mustard" aria-expanded="false"><div class="filter-option"><div class="filter-option-inner"><div class="filter-option-inner-inner">Mustard</div></div> </div></button><div class="dropdown-menu" role="combobox" x-placement="top-start" style="max-height: 224px; overflow: hidden; min-height: 0px; position: absolute; transform: translate3d(0px, -116px, 0px); top: 0px; left: 0px; will-change: transform;"><div class="inner show" role="listbox" aria-expanded="false" tabindex="-1" style="max-height: 206px; overflow-y: auto; min-height: 0px;"><ul class="dropdown-menu inner show"><li class="selected active"><a role="option" class="dropdown-item selected active" aria-disabled="false" tabindex="0" aria-selected="true"><span class="fa bs-ok-default check-mark"></span><span class="text">Mustard</span></a></li><li><a role="option" class="dropdown-item" aria-disabled="false" tabindex="0" aria-selected="false"><span class="fa bs-ok-default check-mark"></span><span class="text">Ketchup</span></a></li><li><a role="option" class="dropdown-item" aria-disabled="false" tabindex="0" aria-selected="false"><span class="fa bs-ok-default check-mark"></span><span class="text">Relish</span></a></li></ul></div></div></div>
+          </table>                    
           <?php
       			if ($product['status'] == 'Available'){
       				echo '<form method="post" action="'.site_url('borrow/play').'">
-                <select class="selectpicker" data-style="btn-success" name="num">
-                  <option>
-                    a week
-                  </option> 
-                  <option>
-                    two weeks
-                  </option>
-                  <option>
-                    three weeks                    
-                  </option> 
-                  <option>
-                    a month
-                  </option> 
-                </select>
+              <div class="dropdown bootstrap-select dropup">
+              <select class="selectpicker" tabindex="-98" name="num">
+                <option>a week</option>
+                <option>two weeks</option>
+                <option>three weeks</option>
+                <option>a month</option>
+              </select><button type="button" class="btn dropdown-toggle btn-light" data-toggle="dropdown" role="button" title="Mustard" aria-expanded="false"><div class="filter-option"><div class="filter-option-inner"><div class="filter-option-inner-inner">Mustard</div></div> </div></button><div class="dropdown-menu" role="combobox" x-placement="top-start" style="max-height: 224px; overflow: hidden; min-height: 0px; position: absolute; transform: translate3d(0px, -116px, 0px); top: 0px; left: 0px; will-change: transform;"><div class="inner show" role="listbox" aria-expanded="false" tabindex="-1" style="max-height: 206px; overflow-y: auto; min-height: 0px;"><ul class="dropdown-menu inner show"><li class="selected active"><a role="option" class="dropdown-item selected active" aria-disabled="false" tabindex="0" aria-selected="true"><span class="fa bs-ok-default check-mark"></span><span class="text">Mustard</span></a></li><li><a role="option" class="dropdown-item" aria-disabled="false" tabindex="0" aria-selected="false"><span class="fa bs-ok-default check-mark"></span><span class="text">Ketchup</span></a></li><li><a role="option" class="dropdown-item" aria-disabled="false" tabindex="0" aria-selected="false"><span class="fa bs-ok-default check-mark"></span><span class="text">Relish</span></a></li></ul></div></div>
+              </div>                
                 <button name="btn" class="btn btn-success" value="'.$product['bookid'].'"> Borrow </button>                
               </form>';
       			}
