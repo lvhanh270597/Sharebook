bbb9888b1ab0b7bc698ac539f5c8b36d0f93dcaa
lvhanh270597 <lvhanh.270597@gmail.com>
2018-11-20 15:52:11 +0700

    add

--- application/views/header_member.php
+++ application/views/header_member.php
@@ -10,13 +10,6 @@
     <title> <?php echo $front_end['title']; ?></title>
 
     <!-- Bootstrap core CSS -->
-
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-
-    <!-- Bootstrap core CSS -->
-
-    <link href="<?php echo base_url('themes/theme1/vendor/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
     <?php
       include_once "page_data_class.php";
       $page_data = new Page_Data();
@@ -25,9 +18,7 @@
       $page_data->show_css();
     ?>    
     <link href="<?php echo base_url().'themes/theme3/'; ?>css/one-page-wonder.min.css" rel="stylesheet">
-    <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
-    <!-- Material Design Bootstrap -->
-    <link href="<?php echo base_url('themes/mdb.min.css'); ?>" rel="stylesheet">      
+    <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>     
   </head>
   <body>
 
