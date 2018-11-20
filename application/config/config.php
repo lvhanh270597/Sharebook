67b628fedc3e85641f669dea7f2a3a897ab0e1b9
lvhanh270597 <lvhanh.270597@gmail.com>
2018-11-20 13:34:05 +0700

    fix database error

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/sharebook/';
+$config['base_url'] = 'http://188.166.18.39/';
 
 /*
 |--------------------------------------------------------------------------
