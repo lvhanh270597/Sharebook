636d81375eeca41700b7965d1848f52842980a37
lvhanh270597 <lvhanh.270597@gmail.com>
2018-11-20 15:53:33 +0700

    fix contact

--- application/views/contact.php
+++ application/views/contact.php
@@ -19,13 +19,13 @@
           <img class="img-fluid rounded mb-4" src="https://scontent.fsgn5-4.fna.fbcdn.net/v/t1.0-9/12795466_1988308704726905_3518290930245502996_n.jpg?_nc_cat=0&amp;oh=8f7374694557d3935548433d85dd19e4&amp;oe=5B69F120" alt="">
         </div>
         <div class="col-lg-6">
-          <h2>Lê Văn Hạnh</h2>          
+          <h2>Sharebook</h2>          
           <ul style="list-style-type:none">
             <li>
               <p>
-                <a href="https://www.facebook.com/hanh2705" ><i class="fab fa-facebook-square" style="font-size:24px"></i> Le Van Hanh</a>
+                <a href="https://www.facebook.com/hanh2705" ><i class="fab fa-facebook-square" style="font-size:24px"></i> Sharebook</a>
               </p>
-              <p> <i class="fas fa-phone-square" style="font-size:24px"></i> 01653001562 </p>
+              <p> <i class="fas fa-phone-square" style="font-size:24px"></i> 0353001562 </p>
               <p> <i class="fas fa-map-marker" style="font-size:24px"></i> D6, KTX B - DHQG TP.HCM</p>
             </li>
           </ul>
